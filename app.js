c7e8749a044ba28d94b5738793e5afc0a1a9c1c5
Zoltan Horvath <zoltan.horvath1990@gmail.com>
2021-01-15 17:42:37 +0100

    working on the onload setup

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("It is wired")
+// Creating the table for onload
+
+const setUpCells = 36;
+
+document.body.onload = addElements;
+
+function addElements () {
+
+  for (let i = 0; i < setUpCells; i++){
+    const newDiv = document.createElement('div');
+    const newContent = document.createTextNode(`${Math.floor(Math.random() * 100)}`);
+    newDiv.appendChild(newContent);
+    const gridWrapper = document.querySelector('.grid-wrapper');
+    gridWrapper.appendChild(newDiv);
+  }
+  
+}
+
