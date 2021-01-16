2faa5b69f9c34fb9aefdb6605bafa69f2c38ec20
Zoltan Horvath <zoltan.horvath1990@gmail.com>
2021-01-16 22:05:37 +0100

    add some styling and base functionality

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ function addElements () {
 
   for (let i = 0; i < setUpCells; i++){
     const newDiv = document.createElement('div');
-    const newContent = document.createTextNode(`${Math.floor(Math.random() * 100)}`);
+    const shuffle = () => {
+      const randomize = Math.floor(Math.random() * 10) * 2;
+        return randomize;
+    };
+    const newContent = document.createTextNode(`${shuffle()}`);
     newDiv.appendChild(newContent);
     const gridWrapper = document.querySelector('.grid-wrapper');
     gridWrapper.appendChild(newDiv);
+    newDiv.classList.add('grid-item');
+
   }
   
 }
 
+
+// Clicking on elements
+
+const clicked = document.addEventListener('click', (event) => {
+   console.log(event);
+});
