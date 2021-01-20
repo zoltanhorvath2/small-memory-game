037077b1ec07a3c198450169d83db2dac51b2484
Zoltan Horvath <zoltan.horvath1990@gmail.com>
2021-01-20 19:19:45 +0100

    add some nice flip effect

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Creating the table for onload
+// CREEATING THE TABLE FOR ONLOAD
 
 const setUpCells = 36;
 const halfOfCells = setUpCells/2;
@@ -20,22 +20,33 @@ tableMaker = () => {
   const shuffledArr = arrayMaker();
 
   for (let i = 0; i < shuffledArr.length; i++){
-    // Creating the grid-items and the content
-
+    // Creating the cell-wrappers and the grid-items
     const newDiv = document.createElement('div');
-    const wrapper = document.querySelector('.grid-wrapper');
-    wrapper.appendChild(newDiv);
-    const newContent = document.createTextNode(`${shuffledArr[i]}`);
-    newDiv.appendChild(newContent);
-
-    // Add the .grit-item class to it
-    
-    newDiv.classList.add('grid-item');
-
+    newDiv.innerHTML = `
+        <div class="wrapper">
+          <div class="card">
+            <div class="backside">${shuffledArr[i]}</div>
+            <div class="frontside"></div>
+          </div>
+        </div>
+    `;
+    newDiv.classList.add('cell-container');
+    const generalWrapper = document.querySelector('.grid-wrapper');
+    generalWrapper.appendChild(newDiv);
   }
 }
 
 tableMaker();
+
+
+// CHECKING AND HIDING THE PAIRS
+
+
+
+
+
+
+
 
 
 
